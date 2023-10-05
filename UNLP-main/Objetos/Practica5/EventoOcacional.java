cb1d29a1e5eb0b33380ee6235de14e96fc9b6c9b
Francisco Lima <110209216+franciscolima05@users.noreply.github.com>
2023-10-05 09:43:06 -0300

    Add files via upload

---
+++ UNLP-main/Objetos/Practica5/EventoOcacional.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica5;
+
+/**
+ *
+ * @author Francisco
+ */
+public class EventoOcacional extends Recital{
+    private String motivo;
+    private String nombreContratante;
+    private int dia;
+    
+    public EventoOcacional(String mot, String nom, int dia, String nombreRecital, int cant, int cantMax){
+        super(nombreRecital, cant, cantMax);
+        this.motivo = mot;
+        this.dia = dia;
+        this.nombreContratante = nom;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public String getNombreContratante() {
+        return nombreContratante;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+    
+    public boolean HayLugar(){
+        return this.getCantTemas() < this.getMaxTemas();
+    }
+@Override
+    public void agregarTema(String tema){
+        if(this.HayLugar()){
+            this.getListaTemas()[this.getCantTemas() + 1] = tema;
+        }
+    }
+    
+    
+@Override
+    public String Actuar(){
+        String aux;
+        if(this.getMotivo().equals("Beneficencia")){
+            aux = "Recuereden Colaborar con " + this.getNombreContratante();
+        }
+        else{
+            if(this.getMotivo().equals("TV")){
+                aux = "Saludos amigos televidentes";
+            }
+            else{
+                aux = "Un Feliz Cumpleaños para " + this.getNombreContratante();
+                }
+        }
+        return aux + super.Actuar();
+    }
+@Override
+    public int CalcularCosto(){
+        int aux;
+        if(this.getMotivo().equals("Beneficencia")){
+            aux = 0;
+        }
+        else{
+            if(this.getMotivo().equals("TV")){
+                aux = 50000;
+            }
+            else{
+                aux = 150000;
+                }
+        }
+        return aux;
+    }
+    
+}
