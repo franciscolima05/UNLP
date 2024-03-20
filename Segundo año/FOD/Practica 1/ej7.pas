3b411e5a2ab371348a004ef813e9d33b83408403
Francisco Lima <110209216+franciscolima05@users.noreply.github.com>
2024-03-20 18:57:50 +0100

    Add files via upload

---
+++ Segundo año/FOD/Practica 1/ej7.pas
@@ -0,0 +1,67 @@
+program ej7;
+type 
+	novela = record
+		nom : string;
+		code : integer;
+		gen : string;
+		precio : real;
+		end;
+		
+	arch = file of novela;
+	
+procedure cargarArchivo(var al : arch);
+var
+	n : novela;
+	te : text;
+begin
+	assign(al, 'ruta');
+	assign(te, 'ruta');
+	rewrite(al);
+	reset(te);
+	while(not EOF(te)) do
+		begin
+			with n do
+				begin
+					readln(te, code, precio, gen);
+					readln(te, nom);
+				end;
+			write(al, n);
+		end;
+	close(al);
+	close(te);
+end;
+
+procedure cargarNovela(var n : novela);
+begin
+	with n do 
+		begin
+			readln(nom);
+			readln(precio);
+			readln(gen);
+			readln(code);
+		end;
+end;
+
+
+procedure agregarNovela(var al : arch);
+var
+
+	n : novela;
+	c : integer;
+begin
+	reset(al)
+	cargarNovela(n);  //se podria hacer comprobando por codigo de novela que la novela no se encontrara en el archivo
+	seek(al, filesize);
+	write(al, n);
+	close(al);
+end;
+
+procedure modificarNovela(var al : arch);
+var
+	n : novela;
+	co : integer;
+begin
+	writeln('ingrese codigo');
+	readln(co);
+	reset(al);
+
